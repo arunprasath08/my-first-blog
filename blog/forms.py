0b468bf559ce48f205498f11064c5c0fb8871704
arunprasath08 <arun.prasath509@gmail.com>
2021-06-11 01:44:40 +0530

    Finishing my blog by adding edit option for existing posts using Django Forms

---
+++ blog/forms.py
@@ -0,0 +1,10 @@
+from typing import ClassVar
+from django import forms
+from django.db.models import fields
+from .models import Post
+
+class PostForm(forms.ModelForm):
+
+    class Meta:
+        model=Post
+        fields=('title','text',)
